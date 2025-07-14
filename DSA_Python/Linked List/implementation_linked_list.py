36698b4326e1699395a76c737aae0e3dca8653c7
Keshtech2002 <keshtech2002@gmail.com>
2025-07-14 14:20:48 +0100

    implement linked list

---
+++ DSA_Python/Linked List/implementation_linked_list.py
@@ -0,0 +1,34 @@
+class Node:
+    def __init__(self, data=None, next=None):
+        self.data = data
+        self.next = next
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_beginning(self, data):
+        node = Node(data, self.head)
+        self.head = node
+
+    def print(self):
+        if self.head is None:
+            print("Linked list is empty")
+            return
+        
+        iterator = self.head
+        linked_list_string = ""
+
+        while iterator:
+            linked_list_string += str(iterator.data) + "-->"
+            iterator = iterator.next
+
+        print(linked_list_string)
+
+        
+
+if __name__ == "__main__":
+    ll = LinkedList()
+    ll.insert_at_beginning(5)
+    ll.insert_at_beginning(9)
+    ll.print()
